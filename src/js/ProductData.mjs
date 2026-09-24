27bc5ab98b89ac8e76bc6211ce739cd7c0be62ee
Tom Emem <tomemem361@gmail.com>
2026-09-24 01:10:21 +0100

    Complete WDD 330 Individual Activity 3

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,15 +9,18 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.baseURL = import.meta.env.VITE_SERVER_URL;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData(category) {
+    const response = await fetch(`${this.baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
+
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(`${this.baseURL}product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
 }
